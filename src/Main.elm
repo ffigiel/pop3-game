c2dc5299251e2060151ee53ade92b33c0bd20ad8
Filip Figiel <figiel.filip@gmail.com>
2021-11-06 17:26:43 +0100

    fix: display the board

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,17 @@
 module Main exposing (main)
 
+import Array exposing (Array)
 import Browser
 import Html as H exposing (Html)
+import Html.Attributes as HA
+
+
+
+-- UTILS
+
+
+type alias Array2d a =
+    Array (Array a)
 
 
 
@@ -27,14 +37,30 @@ type alias Flags =
 
 
 type alias Model =
-    {}
+    { board : Array2d Piece
+    }
+
+
+type Piece
+    = Red
+    | Green
+    | Blue
 
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     let
         model =
-            {}
+            { board = board
+            }
+
+        board =
+            [ [ Red, Green, Green ]
+            , [ Red, Green, Blue ]
+            , [ Blue, Green, Blue ]
+            ]
+                |> List.map Array.fromList
+                |> Array.fromList
 
         cmd =
             Cmd.none
@@ -63,5 +89,29 @@ subscriptions _ =
 
 
 view : Model -> Html Msg
-view _ =
-    H.text ""
+view model =
+    let
+        viewRow : Array Piece -> Html Msg
+        viewRow row =
+            H.div []
+                (Array.toList row
+                    |> List.map viewPiece
+                )
+    in
+    H.div [ HA.class "gameBoard" ]
+        (Array.toList model.board
+            |> List.map viewRow
+        )
+
+
+viewPiece : Piece -> Html Msg
+viewPiece piece =
+    case piece of
+        Red ->
+            H.div [ HA.class "gamePiece -red" ] []
+
+        Green ->
+            H.div [ HA.class "gamePiece -green" ] []
+
+        Blue ->
+            H.div [ HA.class "gamePiece -blue" ] []
