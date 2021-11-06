29bf35a86c9b4aa4d238e066401ba717e980af72
Filip Figiel <figiel.filip@gmail.com>
2021-11-06 18:57:43 +0100

    fix: find neighbors on click

--- src/Main.elm
+++ src/Main.elm
@@ -8,14 +8,6 @@ import Html.Events as HE
 
 
 
--- UTILS
-
-
-type alias Array2d a =
-    Array (Array a)
-
-
-
 -- MAIN
 
 
@@ -38,9 +30,17 @@ type alias Flags =
 
 
 type alias Model =
-    { board : Array2d Piece
+    { board : Board
     , debug : String
     }
+
+
+type alias Board =
+    Array2d Piece
+
+
+type alias Array2d a =
+    Array (Array a)
 
 
 type Piece
@@ -76,18 +76,96 @@ init _ =
 
 
 type Msg
-    = ClickedPiece ( Int, Int )
+    = ClickedPiece Piece ( Int, Int )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ClickedPiece ( x, y ) ->
+        ClickedPiece piece ( x, y ) ->
             let
                 debug =
-                    "Clicked " ++ String.fromInt x ++ ", " ++ String.fromInt y
+                    "Clicked "
+                        ++ String.fromInt x
+                        ++ ", "
+                        ++ String.fromInt y
+                        ++ "\n"
+                        ++ "Found "
+                        ++ String.fromInt (List.length results)
+                        ++ " matches: "
+                        ++ Debug.toString results
+
+                results =
+                    neighborsOfSameColor piece ( x, y ) model.board
             in
             ( { model | debug = debug }, Cmd.none )
+
+
+type alias BoardSearch =
+    Array2d { piece : Piece, visited : Bool }
+
+
+neighborsOfSameColor : Piece -> ( Int, Int ) -> Board -> List ( Int, Int )
+neighborsOfSameColor piece ( x, y ) board =
+    let
+        boardSearch : BoardSearch
+        boardSearch =
+            board
+                |> Array.map
+                    (\r ->
+                        r
+                            |> Array.map
+                                (\p ->
+                                    { piece = p
+                                    , visited = False
+                                    }
+                                )
+                    )
+
+        ( _, result ) =
+            neighborsOfSameColorHelper piece ( x, y ) ( boardSearch, [] )
+    in
+    result
+
+
+neighborsOfSameColorHelper :
+    Piece
+    -> ( Int, Int )
+    -> ( BoardSearch, List ( Int, Int ) )
+    -> ( BoardSearch, List ( Int, Int ) )
+neighborsOfSameColorHelper piece ( x, y ) ( boardSearch, results ) =
+    let
+        found =
+            boardSearch
+                |> Array.get y
+                |> Maybe.andThen (Array.get x)
+                |> Maybe.andThen (\t -> Just <| not t.visited && t.piece == piece)
+                |> Maybe.withDefault False
+
+        newBoardSearch =
+            boardSearch
+                |> Array.indexedMap
+                    (\ty row ->
+                        row
+                            |> Array.indexedMap
+                                (\tx t ->
+                                    if ty == y && tx == x then
+                                        { t | visited = True }
+
+                                    else
+                                        t
+                                )
+                    )
+    in
+    if found then
+        ( newBoardSearch, ( x, y ) :: results )
+            |> neighborsOfSameColorHelper piece ( x + 1, y )
+            |> neighborsOfSameColorHelper piece ( x - 1, y )
+            |> neighborsOfSameColorHelper piece ( x, y + 1 )
+            |> neighborsOfSameColorHelper piece ( x, y - 1 )
+
+    else
+        ( newBoardSearch, results )
 
 
 subscriptions : Model -> Sub Msg
@@ -136,6 +214,6 @@ viewPiece y x piece =
     H.button
         [ HA.class "gamePiece"
         , HA.class colorClass
-        , HE.onClick <| ClickedPiece ( y, x )
+        , HE.onClick <| ClickedPiece piece ( x, y )
         ]
         []
