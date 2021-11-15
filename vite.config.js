66e78c7e52baed7124634f91cee21ab36ba550ba
Filip Figiel <figiel.filip@gmail.com>
2021-11-16 00:13:56 +0100

    fix: add manifest

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite"
 import elmPlugin from "vite-plugin-elm"
 
 export default defineConfig({
+  build: {
+    publicDir: 'public'
+  },
   plugins: [
     elmPlugin({ debug: false }),
   ],
